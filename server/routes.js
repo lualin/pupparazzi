4c3f3ad5cbae7d7dd47afd8ffed849046b10e5b3
Sean Kim <ksiw0427@gmail.com>
2023-03-01 16:09:15 +1300

    edit working

---
+++ server/routes.js
@@ -0,0 +1,64 @@
+// Require calls
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const puppiesData = require('./data/data.json')
+
+// Router setting
+const router = express.Router()
+
+// Data path setting
+const dataPath = path.join(__dirname, '..', '/server/data', 'data.json')
+
+// '/puppies/:id', id paramter given to find a puppy with same id as the param
+router.get('/:id', (req, res) => {
+  // Find from puppies json file where the id params matches to puppy id
+  const puppyObj = puppiesData['puppies'].find(
+    (elem) => elem.id === parseInt(req.params.id)
+  )
+  res.render('details', puppyObj)
+})
+
+// '/puppies/:id/edit, get edit view page to edit the props of puppy
+router.get('/:id/edit', (req, res) => {
+  const editObj = puppiesData['puppies'].find(
+    (elem) => elem.id === parseInt(req.params.id)
+  )
+  res.render('edit', editObj)
+})
+
+// When submit buttons is clicked, it updates the props of puppy
+router.post('/:id/edit', (req, res) => {
+  const puppyId = parseInt(req.params.id) // parameter to integer
+  const updatedName = req.body.name // get name from input
+  const updatedBreed = req.body.breed // get breed from input
+  const updatedOwner = req.body.owner // get owner from input
+
+  // Find the index of the puppy object in the array
+  const puppyIndex = puppiesData['puppies'].findIndex(
+    (elem) => elem.id === puppyId // find puppy id that matches the param given
+  )
+
+  // Update the puppy object with the new name
+  if (puppyIndex !== -1) {
+    puppiesData['puppies'][puppyIndex].name = updatedName // index given and update the puppy name
+    puppiesData['puppies'][puppyIndex].breed = updatedBreed // index given and update the puppy breed
+    puppiesData['puppies'][puppyIndex].owner = updatedOwner // index given and update the puppy owner
+
+    // Write the updated data back to the file
+    fs.writeFile(dataPath, JSON.stringify(puppiesData), (err) => {
+      if (err) throw err
+      // if error incurs, throw error
+      // or console.log successful msg
+      console.log('Data updated.')
+    })
+
+    // Redirect the user back to the details page for the updated puppy
+    res.redirect(`/puppies/${puppyId}`)
+  } else {
+    // if puppy id is not valid, head to 404 error page
+    res.status(404).send('Puppy not found')
+  }
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,71 +1,25 @@
-// const { Router } = require('express')
-// const express = require('express')
-// const hbs = require('express-handlebars')
-// const fsPromises = require('fs').promises
-// const puppies = require('./data/data.json')
-
-// const server = express()
-
-// // Router variables
-// const homeRouter = express.Router()
-
-// // Server configuration
-// const publicFolder = __dirname + '/public'
-// const viewFolder = __dirname + '/views'
-// server.use(express.static(publicFolder))
-// server.use(express.urlencoded({ extended: false }))
-
-// // Handlebars configuration
-// server.engine('hbs', hbs.engine({ extname: 'hbs' }))
-// server.set('view engine', 'hbs')
-// server.set('views', __dirname + '/views')
-
-// // Your routes/router(s) should go here
-// homeRouter.get('/', (req, res) => {
-//   let filename = viewFolder + '/home'
-//   res.render(filename, (err) => {
-//     if (err) {
-//       console.log(err)
-//     } else {
-//       console.log(`Sent: ${filename} \n '/' router working`)
-//       res.end()
-//     }
-//   })
-// })
-
-// // Server use router
-
-// server.use(homeRouter)
-
-// module.exports = server
-// module.exports = { Router }
-
 const express = require('express')
 const hbs = require('express-handlebars')
-const puppiesData = require('./data/data.json')
+const puppiesRoute = require('./routes')
+const puppies = require('./data/data.json')
 
-const server = express()
-// const router = express.Router()
+const app = express()
+module.exports = app
 
 // Server configuration
 const publicFolder = __dirname + '/public'
-server.use(express.static(publicFolder))
-server.use(express.urlencoded({ extended: false }))
+app.use(express.static(publicFolder))
+app.use(express.urlencoded({ extended: false }))
 
 // Handlebars configuration
-server.engine('hbs', hbs.engine({ extname: 'hbs' }))
-server.set('view engine', 'hbs')
-server.set('views', __dirname + '/views')
+app.engine('hbs', hbs.engine({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
 
-// Your routes/router(s) should go here
-server.get('/', (req, res) => {
-  //   const viewData = JSON.parse(JSON.stringify(puppiesData))
-  //   const puppies = {
-  //     id: viewData.puppies.id,
-  //     name: viewData.puppies.name,
-  //     image: viewData.puppies.image,
-  //   }
-  res.render('home', puppiesData)
+// Use puppies router for /puppies/:id route
+app.use('/puppies', puppiesRoute)
+
+// Home route
+app.get('/', async (req, res) => {
+  await res.render('home', puppies)
 })
-
-module.exports = server
