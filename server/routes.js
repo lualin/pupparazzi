1fd88f225d9a3eb4e070be90d432754053e7aa5c
Sean Kim <ksiw0427@gmail.com>
2023-03-01 18:39:54 +1300

    css adjusted and add new pet feature added but not working

--- server/routes.js
+++ server/routes.js
@@ -3,12 +3,35 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const puppiesData = require('./data/data.json')
+// const { route } = require('./server')
 
 // Router setting
 const router = express.Router()
 
 // Data path setting
 const dataPath = path.join(__dirname, '..', '/server/data', 'data.json')
+
+router.post('/add', (req, res) => {
+  const newID = puppiesData['puppies'].length + 1
+  const newName = req.body.name
+  const newBreed = req.body.breed
+  const newOwner = req.body.owner
+  const newPetObj = {
+    id: newID,
+    name: newName,
+    owner: newOwner,
+    image: '',
+    breed: newBreed,
+    _locals: {},
+  }
+
+  try {
+    puppiesData['puppies'].push(newPetObj)
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 // '/puppies/:id', id paramter given to find a puppy with same id as the param
 router.get('/:id', (req, res) => {
@@ -20,15 +43,15 @@ router.get('/:id', (req, res) => {
 })
 
 // '/puppies/:id/edit, get edit view page to edit the props of puppy
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', async (req, res) => {
   const editObj = puppiesData['puppies'].find(
     (elem) => elem.id === parseInt(req.params.id)
   )
-  res.render('edit', editObj)
+  await res.render('edit', editObj)
 })
 
 // When submit buttons is clicked, it updates the props of puppy
-router.post('/:id/edit', (req, res) => {
+router.post('/:id/edit', async (req, res) => {
   const puppyId = parseInt(req.params.id) // parameter to integer
   const updatedName = req.body.name // get name from input
   const updatedBreed = req.body.breed // get breed from input
@@ -41,20 +64,35 @@ router.post('/:id/edit', (req, res) => {
 
   // Update the puppy object with the new name
   try {
+    // console.log changes made
+    // pet name change
+    console.log('-----------<Name>-----------')
+    console.log(`Before: ${puppiesData['puppies'][puppyIndex].name}`)
     puppiesData['puppies'][puppyIndex].name = updatedName // index given and update the puppy name
+    console.log(`After: ${updatedName} \n`)
+
+    // pet breed change
+    console.log('-----------<Breed>-----------')
+    console.log(`Before: ${puppiesData['puppies'][puppyIndex].breed}`)
     puppiesData['puppies'][puppyIndex].breed = updatedBreed // index given and update the puppy breed
+    console.log(`After: ${updatedBreed} \n`)
+
+    // pet owner change
+    console.log('-----------<Owner>-----------')
+    console.log(`Before: ${puppiesData['puppies'][puppyIndex].owner}`)
     puppiesData['puppies'][puppyIndex].owner = updatedOwner // index given and update the puppy owner
+    console.log(`After: ${updatedOwner} \n`)
 
     // Write the updated data back to the file
     fs.writeFile(dataPath, JSON.stringify(puppiesData), (err) => {
       if (err) throw err
       // if error incurs, throw error
       // or console.log successful msg
-      console.log('Data updated.')
+      console.log('All data updated.')
     })
 
     // Redirect the user back to the details page for the updated puppy
-    res.redirect(`/puppies/${puppyId}`)
+    await res.redirect(`/puppies/${puppyId}`)
   } catch {
     // if puppy id is not valid, head to 404 error page
     res.status(404).send('Puppy not found')
